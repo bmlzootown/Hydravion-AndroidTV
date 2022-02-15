394df990c6b00bc0a5b9c74f2529fc78913e7099
bmlzootown <bmlzootown@gmail.com>
2022-02-15 18:07:03 -0500

    Parse description HTML on detail screen

--- app/src/main/java/ml/bmlzootown/hydravion/detail/DetailsDescriptionPresenter.kt
+++ app/src/main/java/ml/bmlzootown/hydravion/detail/DetailsDescriptionPresenter.kt
@@ -1,43 +1,30 @@
-package ml.bmlzootown.hydravion.detail;
+package ml.bmlzootown.hydravion.detail
 
-import android.annotation.SuppressLint;
+import androidx.leanback.widget.AbstractDetailsDescriptionPresenter
+import android.annotation.SuppressLint
+import androidx.core.text.parseAsHtml
+import ml.bmlzootown.hydravion.models.Video
+import org.ocpsoft.prettytime.PrettyTime
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
 
-import androidx.leanback.widget.AbstractDetailsDescriptionPresenter;
-
-import org.ocpsoft.prettytime.PrettyTime;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
-import java.util.Locale;
-import java.util.TimeZone;
-
-import ml.bmlzootown.hydravion.models.Video;
-
-public class DetailsDescriptionPresenter extends AbstractDetailsDescriptionPresenter {
-
-    @Override
-    protected void onBindDescription(ViewHolder viewHolder, Object item) {
-        Video vid = (Video) item;
-
-        if (vid != null) {
-            @SuppressLint("SimpleDateFormat") SimpleDateFormat input = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
-            input.setTimeZone(TimeZone.getTimeZone("UTC"));
-            Date date = null;
-            try {
-                date = input.parse(vid.getReleaseDate());
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-            PrettyTime p = new PrettyTime();
-            String elapsed = p.format(date);
-
-            viewHolder.getTitle().setText(vid.getTitle());
-            //viewHolder.getSubtitle().setText(vid.getReleaseDate());
-            viewHolder.getSubtitle().setText(elapsed);
-            viewHolder.getBody().setText(vid.getDescription());
+class DetailsDescriptionPresenter : AbstractDetailsDescriptionPresenter() {
+    override fun onBindDescription(viewHolder: ViewHolder, item: Any) {
+        val vid = item as Video
+        @SuppressLint("SimpleDateFormat") val input = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+        input.timeZone = TimeZone.getTimeZone("UTC")
+        var date: Date? = null
+        try {
+            date = input.parse(vid.releaseDate)
+        } catch (e: ParseException) {
+            e.printStackTrace()
         }
+        val p = PrettyTime()
+        val elapsed = p.format(date)
+        viewHolder.title.text = vid.title
+        //viewHolder.getSubtitle().setText(vid.getReleaseDate());
+        viewHolder.subtitle.text = elapsed
+        viewHolder.body.text = vid.description.parseAsHtml()
     }
 }
